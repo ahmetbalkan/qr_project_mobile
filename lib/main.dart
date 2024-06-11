84dba461752a6d99efacc62e253f7198b1b680bf
Ahmet R. BALKAN <48199986+ahmetbalkan@users.noreply.github.com>
2024-06-11 04:30:41 +0300

    re-commited.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qontact/core/utils/locale/product_localization.dart';
+import 'package:qontact/locator.dart';
+import 'package:qontact/presentation/theme/app_color.dart';
 import 'package:qontact/qr_project.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const QrProject());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  setupLocator();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: AppColors.primary,
+  ));
+  runApp(ProductLocalization(child: const ProviderScope(child: QrProject())));
 }
--- lib/qr_project.dart
+++ lib/qr_project.dart
@@ -1,13 +1,43 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:qontact/features/splash_page/splash_page.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:qontact/locator.dart';
+import 'package:qontact/presentation/navigation/app_router.dart';
+import 'package:qontact/presentation/theme/theme_notifier.dart';
 
-class QrProject extends StatelessWidget {
+import 'presentation/theme/app_theme.dart';
+
+class QrProject extends ConsumerWidget {
   const QrProject({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashScreen(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider);
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    final rootRouter = locator<RootRouter>();
+
+    return ScreenUtilInit(
+      //Pixel 7 Pro 412px × 892px
+      designSize: const Size(412, 892),
+      builder: (context, child) {
+        return MaterialApp.router(
+          routerConfig: rootRouter.config(),
+          title: 'Flutter Theme Example',
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: themeMode,
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+        );
+      },
     );
   }
 }
